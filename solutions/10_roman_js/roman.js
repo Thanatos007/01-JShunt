7466594783107cafe4ee80af1eb026aa7add0180
thanatos007 <lmayancela08@gmail.com>
2015-10-14 21:54:08 +0000

    Roman

--- solutions/10_roman_js/roman.js
+++ solutions/10_roman_js/roman.js
@@ -1,13 +1,57 @@
-var I = 1
-var V = 5
-var X = 10
-var L = 50
-var C = 100
-var D = 500
-var M = 1000
-var number = prompt("What is your number?")
-
-function roman(){
-    
+function IntegerToRoman(input){
+    if(input < 1 || input > 1000)
+     return "This is an invalid number";
+var s ='';
+while(input >= 1000){
+    s += "M";
+    input -= 1000;
 }
-
+while(input >= 900){
+    s += "CM";
+    input -= 900;
+}
+while(input >= 500){
+    s += "D";
+    input -= 500;
+}
+while(input >= 400){
+    s += "CM";
+    input -= 400;
+}
+while(input >= 100){
+    s += "C";
+    input -= 100;
+}
+while(input >= 90){
+    s += "XC";
+    input -= 90;
+}
+while(input >= 50){
+    s += "L";
+    input -= 50;
+}
+while(input >= 40){
+    s += "XL";
+    input -= 40;
+}
+while(input >= 10){
+    s += "X";
+    input -= 10;
+}
+while(input >= 9){
+    s += "IX";
+    input -= 9;
+}
+while(input >= 5){
+    s += "V";
+    input -= 5;
+}
+while(input >= 4){
+    s += "IV";
+    input -= 4;
+}
+while(input >= 1){
+    s += "I";
+    input -= 1;
+}
+}
